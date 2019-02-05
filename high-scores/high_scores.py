c617dacbaa777a5ced0ae21d94a843703cd6a79d
Isaac <isaac@ktbo.mx>
2019-02-05 17:16:36 -0600

    High Scores Solution

--- high-scores/high_scores.py
+++ high-scores/high_scores.py
@@ -1,3 +1,26 @@
 class HighScores(object):
+
     def __init__(self, scores):
-        pass
+        self.scores = scores
+
+    def latest(self):
+        return self.scores[-1]
+    
+    def personal_best(self):
+        return max(self.scores)
+
+    def personal_top(self):
+        self.scores.sort(reverse = True)
+        return self.scores[:3]
+
+    def report(self):
+        best = self.personal_best()
+        latest = self.latest()
+
+        if latest >= best:
+            message = "Your latest score was {0}. That's your personal best!".format(latest)
+        else:
+            mod_best = best - latest
+            message = "Your latest score was {0}. That's {1} short of your personal best!".format(latest, mod_best)
+
+        return message
