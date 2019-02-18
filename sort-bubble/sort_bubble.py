0702dda7248b7101c073ec23bdd7eeb04b54d64c
Isaac <isaac@ktbo.mx>
2019-02-17 23:57:00 -0600

    Sort Buble

--- sort-bubble/sort_bubble.py
+++ sort-bubble/sort_bubble.py
@@ -1,9 +1,9 @@
 def sort_bubble(array):
   for ind1 in range(len(array)):
-    for ind2 in range(len(array)):
-      if(array[ind2] > array[ind1]):
+    for ind2 in range(len(array) - 1 - ind1):
+      if(array[ind2] > array[ind2+1]):
         tempNum = array[ind2]
-        array[ind2] = array[ind1]
-        array[ind1] = tempNum
+        array[ind2] = array[ind2+1]
+        array[ind2+1] = tempNum
 
   return array
